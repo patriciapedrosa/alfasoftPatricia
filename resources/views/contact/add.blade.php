3aa55fbc9a23b0301b983559e3a90cdb70c97ef1
Patrícia Pedrosa <patricia.pedrosa.1996@hotmail.com>
2020-08-31 18:29:00 +0100

    done

--- resources/views/contact/add.blade.php
+++ resources/views/contact/add.blade.php
@@ -17,25 +17,25 @@
             <input
             type="text" class="form-control"
             name="name" id="name"
-            placeholder="name" value="{{old('name', $contact->name)}}" />
+            placeholder="Nome" value="{{old('name', $contact->name)}}" />
         </div>
         <div class="form-group">
             <label for="contact">Telefone</label>
             <input
             type="text" class="form-control"
             name="contact" id="contact"
-            placeholder="contact" value="{{old('contact', $contact->contact)}}" />
+            placeholder="Contato" value="{{old('contact', $contact->contact)}}" />
         </div>
         <div class="form-group">
             <label for="email">Email</label>
             <input
             type="text" class="form-control"
             name="email" id="email"
-            placeholder="email" value="{{old('email', $contact->email)}}" />
+            placeholder="Email" value="{{old('email', $contact->email)}}" />
         </div>
         <div class="form-group">
-            <button type="submit" class="btn btn-success" id="ok" name="ok">Save</button>
-            <a class="btn btn-default" href="{{route('index')}}" id="cancel">Cancel</a>
+            <button type="submit" class="btn btn-success" id="ok" name="ok">Guardar</button>
+            <a class="btn btn-default" href="{{route('index')}}" id="cancel">Cancelar</a>
         </div> 
         {{csrf_field()}}
     </form>
