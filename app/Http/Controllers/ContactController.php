55c274f93262c349ddcea3d811a9f96c10a001dc
Patrícia Pedrosa <patricia.pedrosa.1996@hotmail.com>
2020-08-31 13:05:24 +0100

    Controller, routes, model

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Contact;
+
+class ContactController extends Controller
+{
+
+	 public function index()
+    {
+        $contacts = Contact::orderBy('id')->paginate(10);
+        return view('contact.index', compact('contacts'));
+    }
+
+    public function create()
+    {
+    	 /*$request->validate([
+                'table_number' => 'required|integer|unique:tables'
+            ]);*/
+        $contato = new Contact();
+        return view('contact.add', compact('contact'));
+    }
+
+    public function delete($id)
+    {
+        $contato = Contact::findOrFail($id);
+        $contato->deleted == 1;
+        $contato->save();
+        /*return redirect()
+            ->route('sensor.list', compact('thing_id'))
+            ->with('success', 'Sensor removido com sucesso');*/
+    }
+
+    
+
+
+
+    /*public function store(Request $request)
+    {
+        $request->validate([
+                'table_number' => 'required|integer|unique:tables'
+            ]);
+        $table = new RestaurantTable();
+        $table->fill($request->all());
+        $table->save();
+        return new TableResource($table);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+                'table_number' => 'required|integer|unique:tables'
+            ]);
+        $table = RestaurantTable::findOrFail($id);
+        $table->update($request->all());
+        return new TableResource($table);
+    }*/
+}
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+Route::get('/','ContactController@index')->name('index');;
